04846ef9ded6a80e141efcde1b6879354e57a5fd
BUSOYE Opegbemi Matthias <busoyeom@gmail.com>
2022-04-11 17:17:22 -0700

    Addind 0-9

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+
+int main (void)
+{
+  char cha;
+
+  for (cha = 'a'; cha <= 'z'; cha++)
+    {
+      putchar(cha);
+    }
+  for (cha = 'A'; ;cha <= 'z'; cha++)
+    {
+      putchar(cha);
+    }
+  putchar(\n);
+
+  return (0);
+
+}
