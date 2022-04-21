9a99ee630bcaa1edbbff75215f536c2ac48084e9
BUSOYE Opegbemi Matthias <busoyeom@gmail.com>
2022-04-21 07:58:22 -0700

    Adding 0 - 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 /**
  * reset_to_98 - a function that changes the value of the input to 98
- * @n: - pinter to int
+ * @n: - pointer to int
  */
 
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
